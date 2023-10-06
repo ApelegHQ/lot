975edda1122e0ca690404815a87a6d3216cee6a8
Ricardo Iván Vieitez Parra <3857362+corrideat@users.noreply.github.com>
2023-10-06 04:04:11 +0200

    Documentation for types and provenance configuration

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,11 +13,37 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+/**
+ * Interface representing a function to perform a task.
+ */
 export interface IPerformTask {
+	/**
+	 * Performs a task.
+	 *
+	 * @param op - The operation to be performed.
+	 * @param args - Variable list of arguments.
+	 * @returns Returns a promise resolving to the result.
+	 */
 	(op: string, ...args: unknown[]): Promise<unknown>;
 }
 
+/**
+ * Interface representing a function to execute a script in a sandboxed
+ * environment.
+ */
 export interface ISandbox {
+	/**
+	 * Executes a script in a sandbox.
+	 *
+	 * @param script - The script to be executed.
+	 * @param allowedGlobals - Optional list of allowed global variables.
+	 * @param externalMethods - Optional dictionary of external methods or
+	 * variables available as globals to the script.
+	 * @param abort - Optional abort signal to terminate the execution.
+	 * @param options - Optional configuration options for the sandbox.
+	 * @returns Returns a promise resolving to a function that can perform
+	 * sandboxed tasks.
+	 */
 	(
 		script: string,
 		allowedGlobals?: string[] | undefined | null,
@@ -27,12 +53,25 @@ export interface ISandbox {
 	): Promise<IPerformTask>;
 }
 
+/**
+ * Type representing the context in which the sandboxed script is executed.
+ */
 export type TContext = Record<PropertyKey, unknown> & {
+	/** The global `this` reference within the context. */
 	globalThis: TContext;
+	/** Represents the commonJS module object with its exports. */
 	module: { exports: unknown };
 };
 
+/**
+ * Type representing the configuration options for the sandbox.
+ */
 export type TSandboxOptions = {
+	/**
+	 * Indicates if the worker should be treated like a browser or node.js worker.
+	 * If true, the worker is a browser worker. Otherwise, it's treated as a node.js worker.
+	 */
 	browserRequireWorker?: boolean;
+	/** The type of the worker to be used (e.g., "classic", "module"). */
 	workerType?: WorkerOptions['type'];
 };
